a22ebfe74554874379ec97dd648d8f7d510b9a19
Duc Le (duclm27) <v.duclm27@vinsmartfuture.tech>
2026-03-11 15:31:53 +0700

    refactor: remove unused redirect component and clean up routing in App.tsx

--- apps/the-cv-web/src/App.tsx
+++ apps/the-cv-web/src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { NavigationLoadingBar, NavigationLoadingProvider } from "./components/navigation-loading-bar";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { GlobalErrorBoundary } from "./pages/share/error.page";
@@ -8,11 +8,6 @@ import LoadingPage from "./pages/share/loading.page";
 const LazyHomePage = lazy(() => import("./pages/home/home.page"));
 const LazyMyResumesPage = lazy(() => import("./pages/my-resumes/my-resumes.page"));
 const LazyBuilderPage = lazy(() => import("./pages/builder/builder.page"));
-
-function RedirectDoc() {
-	const { id } = useParams<{ id: string }>();
-	return <Navigate to={`/doc/${id}`} replace />;
-}
 
 function App() {
 	return (
@@ -25,8 +20,6 @@ function App() {
 							<Route path="/" Component={LazyHomePage} />
 							<Route path="/my-resumes" Component={LazyMyResumesPage} />
 							<Route path="/doc/:id" Component={LazyBuilderPage} />
-							<Route path="/c/:id" element={<RedirectDoc />} />
-							<Route path="/r/:id" element={<RedirectDoc />} />
 						</Routes>
 					</Suspense>
 				</NavigationLoadingProvider>
